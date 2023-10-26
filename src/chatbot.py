2c917bcdb5827fdcf0f75f42dfaab90057228fc5
osakpolor omoregie <oomoregie@academi.rrc.ca>
2023-10-26 11:38:45 -0500

    initial commit: defined get_account and added  unit_testing for get_account

--- src/chatbot.py
+++ src/chatbot.py
@@ -19,81 +19,107 @@ VALID_TASKS = {"balance", "deposit", "exit"}
 
 
 
+def get_account() -> int:
+    """
+    args(): no parameters.
+
+    return(int): return the users_selection if it an integer.
+
+    raises:
+    valueError: if user_selection is not an integer.
+    Exception: if Account number does not exist.
+    """
+
+        #continue if  user_selction is valid.
+    user_selection = input('Please enter your account number: ')
+    try:
+        account_number = int(user_selection)
+        if account_number in ACCOUNTS:
+            return account_number
+        elif account_number not in ACCOUNTS:
+            raise Exception('Acount Number Does not Exsist.')
+    except ValueError:
+        raise ValueError('Account Number must be a whole number.')
+
+
+
+
+
 
 ## GIVEN CHATBOT FUNCTION
 ## REQUIRES REVISION
 """
 def chatbot():
-    '''
-    The main program.  Uses the functionality of the functions:
-        get_account()
-        get_amount()
-        get_balance()
-        make_deposit()
-        user_selection()
-    '''
+'''
+The main program.  Uses the functionality of the functions:
+get_account()
+get_amount()
+get_balance()
+make_deposit()
+user_selection()
+'''
 
-    print("Welcome! I'm the PiXELL River Financial Chatbot!  Let's get chatting!")
+print("Welcome! I'm the PiXELL River Financial Chatbot!  Let's get chatting!")
 
-    keep_going = True
-    while keep_going:
-        try:
-            ## CALL THE user_selection FUNCTION HERE 
-            ## CAPTURING THE RESULTS IN A VARIABLE CALLED
-            ## selection:
-
-
-            if selection != "exit":
-                
-                # Account number validation.
-                valid_account = False
-                while valid_account == False:
-                    try:
-                        ## CALL THE get_account FUNCTION HERE
-                        ## CAPTURING THE RESULTS IN A VARIABLE 
-                        ## CALLED account:
+keep_going = True
+while keep_going:
+try:
+    ## CALL THE user_selection FUNCTION HERE 
+    ## CAPTURING THE RESULTS IN A VARIABLE CALLED
+    ## selection:
 
 
-                        valid_account = True
-                    except Exception as e:
-                        # Invalid account.
-                        print(e)
-                if selection == "balance":
-                        ## CALL THE get_balance FUNCTION HERE
-                        ## PASSING THE account VARIABLE DEFINED 
-                        ## ABOVE, AND PRINT THE RESULTS:
-
-                else:
-
-                    # Amount validation.
-                    valid_amount = False
-                    while valid_amount == False:
-                        try:
-                            ## CALL THE get_amount FUNCTION HERE
-                            ## AND CAPTURE THE RESULTS IN A VARIABLE 
-                            ## CALLED amount:
+    if selection != "exit":
+        
+        # Account number validation.
+        valid_account = False
+        while valid_account == False:
+            try:
+                ## CALL THE get_account FUNCTION HERE
+                ## CAPTURING THE RESULTS IN A VARIABLE 
+                ## CALLED account:
 
 
-                            valid_amount = True
-                        except Exception as e:
-                            # Invalid amount.
-                            print(e)
-                    ## CALL THE make_deposit FUNCTION HERE PASSING THE 
-                    ## VARIABLES account AND amount DEFINED ABOVE AND 
-                    ## PRINT THE RESULTS:
+                valid_account = True
+            except Exception as e:
+                # Invalid account.
+                print(e)
+        if selection == "balance":
+                ## CALL THE get_balance FUNCTION HERE
+                ## PASSING THE account VARIABLE DEFINED 
+                ## ABOVE, AND PRINT THE RESULTS:
+
+        else:
+
+            # Amount validation.
+            valid_amount = False
+            while valid_amount == False:
+                try:
+                    ## CALL THE get_amount FUNCTION HERE
+                    ## AND CAPTURE THE RESULTS IN A VARIABLE 
+                    ## CALLED amount:
 
 
-            else:
-                # User selected 'exit'
-                keep_going = False
-        except Exception as e:
-            # Invalid selection:
-            print(e)
+                    valid_amount = True
+                except Exception as e:
+                    # Invalid amount.
+                    print(e)
+            ## CALL THE make_deposit FUNCTION HERE PASSING THE 
+            ## VARIABLES account AND amount DEFINED ABOVE AND 
+            ## PRINT THE RESULTS:
 
-    print("Thank you for banking with PiXELL River Financial.")
+
+    else:
+        # User selected 'exit'
+        keep_going = False
+except Exception as e:
+    # Invalid selection:
+    print(e)
+
+print("Thank you for banking with PiXELL River Financial.")
 """
-    
+
 """
 if __name__ == "__main__":
-    chatbot()
+chatbot()
 """
--- tests/test_chatbot.py
+++ tests/test_chatbot.py
@@ -6,6 +6,35 @@ Usage:
 """
 import unittest
 from unittest.mock import patch
+from src.chatbot import get_account,VALID_TASKS,ACCOUNTS
 
 class chatbotTests(unittest.TestCase):
-    
+    def test_get_account_valid(self):
+     #builtins.input <-- allows us to mock input
+        #builtins.print <-- would allow us to mock print
+        with patch('builtins.input') as mock_input:
+        #Arrange
+        #the side_effect list below 'mocks' input for valid account number.
+        # values that are prompted for in the function:
+            mock_input.side_effect = ['123456']
+            expected = 123456
+            #Act
+            actual = get_account()
+            #Assert
+            self.assertEqual(expected, actual)
+
+    def test_get_account_invalid(self):
+        #builtins.input <-- allows us to mock input
+        with patch('builtins.input') as mock_input:
+        #Arrange
+        #the side_effect list below 'mocks' input for non numeric number.
+            mock_input.side_effect = ['adsr']
+            expected = 'Account Number must be a whole number.'
+            #Act and Asssert
+            #assertRaies(ValueError) <-- because function raises
+            # ValueError when an invalid acount number is provided.
+            with self.assertRaises(ValueError) as context:
+                get_account()
+            self.assertEqual(str(context.exception), expected)
+            
+
